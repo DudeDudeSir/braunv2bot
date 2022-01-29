731b0569c620fc058bf116440ff3837b1e5aa141
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2022-01-29 15:55:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,15 +4,15 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://https://t.me/RahulrsParu>✯Rahul✯</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href= https://t.me/RahulrsParu>✯Rahul✯</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈:  𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ My Group:<B>♻️@moviecenter225</B>"""
     SOURCE_TXT = """<b>NOTE:</b>
 -  sorry closed source project. 
-- 🕵‍♂Developer -<a href=https://https://t.me/RahulrsParu><I>✯Rahul✯</I></a>
+- 🕵‍♂Developer -<a href= https://t.me/RahulrsParu><I>✯Rahul✯</I></a>
 
 <b>DEVS:</b>
 - <a href=https://t.me/RahulrsParu>✯Rahul✯</a>"""
