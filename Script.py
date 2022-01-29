89418d4d9726cdf32615c01dd26b59c7b741aab2
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2022-01-29 14:25:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,10 +12,10 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 -  sorry closed source project. 
-- Dev- <a href=https://t.me/RahulrsParu><I>✯Rahul✯</I></a>"""
+- Dev
 
 <b>DEVS:</b>
-- <a href=https://t.me/moviecenter225>Group</a>"""
+- <a href=https://t.me/RahulrsParu>Rahul</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
