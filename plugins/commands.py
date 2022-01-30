e03acab98fccd2cec74d5585f717fec4378e4df3
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2022-01-30 20:07:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,11 +44,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('😎 Developer', url='https://t.me/RahulrsParu')
             InlineKeyboardButton('💢 Group', url='https://t.me/moviecenter225')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('❄️ Help', callback_data='help'),
+            InlineKeyboardButton('🎭 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
