c31093ed13ad182dead9e80b94dd5820d4be3e44
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2022-02-04 08:17:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -409,8 +409,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('💢Group', url='https://t.me/moviecenter225')
         ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🔥 Help', callback_data='help'),
+            InlineKeyboardButton('👹 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
