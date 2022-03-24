be43100db86bc0e4bd4ef377634983539218210a
Rahul <88625008+Rahulparu@users.noreply.github.com>
2022-03-24 14:39:27 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🤩 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('👩‍💻 developer', url='https://t.me/RahulrsParu')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
