a234786b03f47cfad3ec38c2ee33c32c69582554
RAHUL <88625008+Rahulparu@users.noreply.github.com>
2022-06-15 15:36:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -618,10 +618,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-        elif query.data == 'reqst1':
-            await query.answer("Hey Bro 😍\n\n🎯 Click On The Button below The Files You Want And Start The Bot ⬇️", True)
+    elif query.data == 'reqst1':
+        await query.answer("Hey Bro 😍\n\n🎯 Click On The Button below The Files You Want And Start The Bot ⬇️", True)
     try: await query.answer('Piracy Is Crime') 
     except: pass
+
 
 
 async def auto_filter(client, msg, spoll=False):
