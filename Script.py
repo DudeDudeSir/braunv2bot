b4dc8a60f0ab7170919da7222b37d666ce80b908
RAHUL <88625008+DudeDudeSir@users.noreply.github.com>
2022-06-28 09:57:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,12 +4,12 @@ class script(object):
 
 ⎆𝐖𝐨𝐫𝐤𝐢𝐧𝐠 𝐖𝐢𝐭𝐡 <a href= https://t.me/moviecenter225><b>MOVIE CENTER...</b></a>
 
- ➪ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝  𝐁𝐲 :<a href= https://t.me/RahulrsParu><I>Rahul</I></a>"""
+ ➪ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝  𝐁𝐲 :<a href= https://t.me/dude_tg><I>Rahul</I></a>"""
 
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href= https://t.me/RahulrsParu>✯Rahul✯</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href= https://t.me/dude_tg>✯Rahul✯</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈:  𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -20,7 +20,7 @@ class script(object):
 - 🕵‍♂Developer -<a href= https://t.me/RahulrsParu><I>✯Rahul✯</I></a>
 
 <b>DEVS:</b>
-- <a href=https://t.me/RahulrsParu>✯Rahul✯</a>"""
+- <a href=https://t.me/dude_tg>✯Rahul✯</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
